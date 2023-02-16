9ced6646f76118006d1eaa5694122874fa6c6caf
KarmanjyotSingh <karmanjyot.singh@students.iiit.ac.in>
2023-02-17 03:34:14 +0530

    Added CPA Secure Encryption Scheme

--- CPA/CPA.py
+++ CPA/CPA.py
@@ -1,33 +1,67 @@
+import sys 
+sys.path.append('../PRG')
+sys.path.append('../PRF')
+from PRG import PRG as prg
+from PRF import PRF as prf
 class CPA:
-    def __init__(self, security_parameter: int, prime_field: int,
-                 generator: int, key: int):
+    # This class implements the encryption scheme that is secured against chosen plain text attack
+    # CPA is deterministic encryption scheme
+    def __init__(self, security_parameter: int, 
+                 prime_field: int,
+                 generator: int, 
+                 key: int):
         """
-        Initialize the values here
-        :param security_parameter: 1ⁿ
-        :type security_parameter: int
-        :param prime_field: q
-        :type prime_field: int
-        :param generator: g
-        :type generator: int
-        :param key: k
-        :type key: int
+        description : implements a CPA encryption scheme
+        ---
+        parameters:
+        ---
+        security_parameter(int) : n (from 1ⁿ)
+        prime_field(int) : p
+        generator(int) : g
+        key(int) : uniformly sampled seed
         """
-        pass
+
+        self.security_parameter = security_parameter
+        self.prime_field = prime_field
+        self.generator = generator
+        self.key = key
+        self.prf = prf(security_parameter = security_parameter,
+                       prime_field = prime_field,
+                       generator = generator,
+                       key = key)
+
 
     def enc(self, message: int, random_seed: int) -> str:
         """
-        Encrypt message against Chosen Plaintext Attack
-        :param message: m
-        :type message: int
-        :param random_seed: r
-        :type random_seed: int
+        description : Encrypt message
+        ---
+        parameters:
+        ---
+        message(int) : m
+        random_seed(int) : r
         """
-        pass
+        
+        n = self.security_parameter
+        # initialise the cipher text
+        cipher_text = self.prf.prg.convert_to_binary(random_seed,n) # n bits
+        Fₖ_r = self.prf.evaluate(random_seed) # Fₖ(r)
+        y = message ^ Fₖ_r
+        cipher_text += self.prf.prg.convert_to_binary(y,n)
+        return cipher_text
+        
 
     def dec(self, cipher: str) -> int:
         """
-        Decrypt ciphertext to obtain plaintext message
-        :param cipher: ciphertext c
-        :type cipher: str
+        description : Decrypt ciphertext to obtain plaintext message
+        ---
+        parameters:
+        ---
+        cipher(str) : c
         """
-        pass
+        r = self.prf.slice_left(cipher)
+        c = self.prf.slice_right(cipher)
+        s = int(c,2)
+        Fₖ_r = self.prf.evaluate(r)
+        m = s ^ Fₖ_r
+        return m
+
